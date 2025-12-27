02bcba08237d07749baf67c88e7bacb44a22ac97
Badhreesh <rao@grandperspective.de>
2025-12-27 13:41:33 +0100

    Initial commit for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,73 @@
+// import "./App.css";
+
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, name: "Milk", done: false },
+  { id: 2, name: "Cheese", done: false },
+  { id: 3, name: "Mushroom", done: false },
+];
+
+function GroceryItem({ item, onDeleteItem }) {
+  return (
+    <li key={item.id}>
+      <input type="checkbox" id={`item-${item.id}`} />
+      <label htmlFor={`item-${item.id}`}>{item.name}</label>
+      <button onClick={() => onDeleteItem(item.id)}>delete</button>
+    </li>
+  );
+}
+
+function GroceryList({ items, onDeleteItem }) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <GroceryItem item={item} onDeleteItem={onDeleteItem} key={item.id} />
+      ))}
+    </ul>
+  );
+}
+
+function ItemForm({ onAddItem }) {
+  const [item, setItem] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const trimmedItem = item.trim();
+    if (trimmedItem.length === 0) return;
+    onAddItem(trimmedItem);
+    setItem("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={item}
+        onChange={(e) => setItem(e.target.value)}
+        placeholder="Enter item..."
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function GroceryListApp() {
+  const [allItems, setAllItems] = useState(initialItems);
+  const addItem = (newItem) =>
+    setAllItems([
+      ...allItems,
+      { id: crypto.randomUUID(), name: newItem, done: false },
+    ]);
+  const deleteItem = (id) =>
+    setAllItems(allItems.filter((item) => item.id !== id));
+  return (
+    <>
+      <h1>Grocery List</h1>
+      <ItemForm onAddItem={addItem} />
+      <GroceryList items={allItems} onDeleteItem={deleteItem} />
+    </>
+  );
+}
+
+export default function App() {
+  return <GroceryListApp />;
+}
