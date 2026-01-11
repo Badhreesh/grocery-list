d04557b79b1a98c41de6554dbea775481d5b955a
Badhreesh <rao@grandperspective.de>
2026-01-11 13:46:11 +0100

    Use flask g and current_app to improve db connection logic

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
-from backend.config import Config
-from backend.database.utils import init_db
+from backend.database.utils import close_db_connection, init_db
+from backend.routes.main import bp
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
-    init_db()
+    with app.app_context():
+        init_db()
+    app.register_blueprint(bp)
+    app.teardown_appcontext(close_db_connection)
+
     return app
--- backend/database/utils.py
+++ backend/database/utils.py
@@ -1,19 +1,26 @@
 import sqlite3
 from pathlib import Path
 
+from flask import current_app, g
+
 schema_path = Path(__file__).parent / "schema.sql"
 db_path = schema_path.parent / "groceries.db"
 
 
 def get_db_connection() -> sqlite3.Connection:
-    conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row
-    return conn
+    if "db_conn" not in g:
+        g.db_conn = sqlite3.connect(db_path)
+        g.db_conn.row_factory = sqlite3.Row
+    return g.db_conn
+
+
+def close_db_connection(e=None) -> None:
+    db_conn = g.pop("db_conn", None)
+    if db_conn is not None:
+        db_conn.close()
 
 
 def init_db() -> None:
     conn = get_db_connection()
-    with open(schema_path, "r") as f:
-        conn.executescript(f.read())
-    conn.commit()
-    conn.close()
+    with current_app.open_resource(schema_path.as_posix()) as f:
+        conn.executescript(f.read().decode())
